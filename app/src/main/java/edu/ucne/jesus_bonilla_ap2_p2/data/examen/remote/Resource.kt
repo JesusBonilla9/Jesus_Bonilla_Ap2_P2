ec651f883304bf71d7b7e50aefbf18bc5a105873
JesusBonilla9 <jesus_bonilla@ucne.edu.do>
2026-07-21 22:21:48 -0400

    Crea package data

---
+++ app/src/main/java/edu/ucne/jesus_bonilla_ap2_p2/data/examen/remote/Resource.kt
@@ -0,0 +1,4 @@
+package edu.ucne.jesus_bonilla_ap2_p2.data.examen.remote
+
+sealed class Resource {
+}
---
+++ app/src/main/java/edu/ucne/jesus_bonilla_ap2_p2/data/examen/repository/ExamenRepositoryImpl.kt
@@ -0,0 +1,5 @@
+package edu.ucne.jesus_bonilla_ap2_p2.data.examen.repository
+
+class ExamenRepositoryImpl{
+
+}
